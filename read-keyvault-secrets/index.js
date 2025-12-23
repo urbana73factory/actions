b789c995d637983d3d8b8bed0dd32b9764bc8ea9
Your Name <you@example.com>
2025-12-23 23:20:22 +0100

    SDFGFSDG

--- read-keyvault-secrets/index.js
+++ read-keyvault-secrets/index.js
@@ -55,10 +55,10 @@ async function run() {
       const envName = name.toUpperCase().replace(/[^A-Z0-9_]/g, '_');
       fs.appendFileSync(
         process.env.GITHUB_ENV,
-        `u73f_${envName}=${value}\n`
+        `UR73FAC_${envName}=${value}\n`
       );
 
-      core.info(`Secret "${name}" exported as ${envName}`);
+      core.info(`Secret "${name}" exported as UR73FAC_${envName}`);
     }
 
   } catch (error) {
